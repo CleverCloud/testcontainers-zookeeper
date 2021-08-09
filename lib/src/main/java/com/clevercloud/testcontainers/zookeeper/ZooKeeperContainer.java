3c7e637b55bd03dfab5f099c7cbdcf201ab016a3
Florentin Dubois <florentin.dubois@clever-cloud.com>
2021-08-09 17:16:05 +0200

    Remove unnecessary logger on ZooKeeperConatiner class

--- lib/src/main/java/com/clevercloud/testcontainers/zookeeper/ZooKeeperContainer.java
+++ lib/src/main/java/com/clevercloud/testcontainers/zookeeper/ZooKeeperContainer.java
@@ -3,8 +3,6 @@
  */
 package com.clevercloud.testcontainers.zookeeper;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
@@ -21,8 +19,6 @@ public class ZooKeeperContainer extends GenericContainer<ZooKeeperContainer> {
     public static final Integer DEFAULT_CLIENT_PORT = 2181;
     public static final Integer DEFAULT_ADMIN_PORT = 8080;
     public static final Duration DEFAULT_STARTUP_TIMEOUT = Duration.ofSeconds(60);
-
-    private final Logger logger = LoggerFactory.getLogger(ZooKeeperContainer.class);
 
     public ZooKeeperContainer() {
         this(DEFAULT_IMAGE_NAME, DEFAULT_STARTUP_TIMEOUT);
