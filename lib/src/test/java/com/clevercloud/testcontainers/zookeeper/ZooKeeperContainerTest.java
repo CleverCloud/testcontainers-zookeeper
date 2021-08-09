854f0fe4c6d4a3b312586eee41769a3f91a5e1bf
Florentin Dubois <florentin.dubois@clever-cloud.com>
2021-08-09 15:51:59 +0200

    Here be dragons

---
+++ lib/src/test/java/com/clevercloud/testcontainers/zookeeper/ZooKeeperContainerTest.java
@@ -0,0 +1,35 @@
+package com.clevercloud.testcontainers.zookeeper;
+
+import org.apache.zookeeper.*;
+import org.junit.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CountDownLatch;
+
+@Testcontainers
+public class ZooKeeperContainerTest {
+    @Container
+    public final ZooKeeperContainer container = new ZooKeeperContainer();
+
+    @Test
+    public void writeFollowedByARead() throws InterruptedException, KeeperException, IOException {
+        container.start();
+
+        final CountDownLatch connectionLatch = new CountDownLatch(1);
+        final ZooKeeper zk = new ZooKeeper(container.getHost(), container.getMappedPort(ZooKeeperContainer.DEFAULT_CLIENT_PORT), event -> {
+            if (event.getState() == Watcher.Event.KeeperState.SyncConnected) {
+                connectionLatch.countDown();
+            }
+        });
+
+        connectionLatch.await();
+
+        zk.create("/test", "test".getBytes(StandardCharsets.UTF_8), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        zk.getData("/test", false, zk.exists("/test", true));
+
+        zk.close();
+    }
+}
